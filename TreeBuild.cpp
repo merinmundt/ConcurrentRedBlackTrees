6d32d74718a17dc7cfedfaa062f8eee6e7ba680d
Merin Mundt <merin@iastate.edu>
2019-12-03 11:37:16 -0600

    initializing redblack tree class, struct and methods

--- TreeBuild.cpp
+++ TreeBuild.cpp
@@ -1,14 +1,31 @@
 #include <pthread.h>
 
+using namespace std;
+enum COLOR {Red, Black};
 class TreeBuild{
 
-    void makeTree(){
-        
-    }
+    struct Node
+    {
+        int data;
+        bool color;
+        Node *left, *right, *parent;
+
+        Node(int data){
+            this->data = data;
+            this->color = Red;
+            right = NULL;
+            left = NULL;
+            parent = NULL;
+        }
+    };
+    
+
+
+
 
     int main(char *file_ptr){
         
-        makeTree();
+        
     }
 
 };
