34edc79b44054790636821b1b8eff2660e5424bb
Merin Mundt <merin@iastate.edu>
2019-12-03 15:38:15 -0600

    WIP

--- TreeBuild.cpp
+++ TreeBuild.cpp
@@ -2,29 +2,46 @@
 
 using namespace std;
 enum COLOR {Red, Black};
-class TreeBuild{
 
-    struct Node
-    {
-        int data;
-        bool color;
-        Node *left, *right, *parent;
+//initializing the node struct
+struct Node
+{
+    int data;
+    bool color;
+    Node *left, *right, *parent;
 
-        Node(int data){
-            this->data = data;
-            this->color = Red;
-            right = NULL;
-            left = NULL;
-            parent = NULL;
+    Node(int data){
+        this->data = data;
+        this->color = Red;
+        right = NULL;
+        left = NULL;
+        parent = NULL;
+    }
+};
+
+class TreeBuild{    
+    private:
+        Node root;
+    public:
+        //This is the constructor
+        TreeBuild(){
+            root = NULL;
         }
-    };
-    
 
+    //Method to insert node in parameter into tree
+    void insertNode(Node *node){
+        if(root == NULL){
+            root = node;
+            return;
+        }
+        
+
+    }
 
 
 
     int main(char *file_ptr){
-        
+        TreeBuild Tree;
         
     }
 
