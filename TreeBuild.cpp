d3b67607854350a9d4ceb3d6e44f301db701ad6e
Merin Mundt <merin@iastate.edu>
2019-12-04 00:25:24 -0600

    not fully sure if this is right

--- TreeBuild.cpp
+++ TreeBuild.cpp
@@ -29,6 +29,47 @@ class TreeBuild{
         }
         
 
+    void LSwitch(Node *node, Node *node1){
+            Node *right = node1->right;
+            node1->right = right->left;
+            if(node1->right != NULL){
+                node1->right->parent = node1;
+            }
+            //moving up
+            right->parent = node1->parent;
+            if(node1->parent == NULL){
+                root = right;
+            }
+            else if(node1->parent->left == node1){
+                node1->parent->left = right;
+            }
+            else{
+                node1->parent->right = right;
+            }
+            right->left = node1;
+            node1->parent = right;
+    }
+
+    void RSwitch(Node *node, Node *node1){
+            Node *left = node1->left;
+            node1->left = left->right;
+            if(node1->left != NULL){
+                node1->left->parent = node1;
+            }
+            //moving up
+            left->parent = node1->parent;
+            if(node1->parent == NULL){
+                root = left;
+            }
+            else if(node1->parent->right == node1){
+                node1->parent->left = left;
+            }
+            else{
+                node1->parent->right = left;
+            }
+            left->right = node1;
+            node1->parent = left;
+    }
 
     //Method to insert node in parameter into tree
     Node* insertNode(Node *node, int data1){
@@ -61,8 +102,26 @@ class TreeBuild{
         //while there is still fixing to be done within the tree
         while((node2->color != Black) && (node2 != node) && (node2->parent->color != Black)){
 
+                parent_ = node2->parent;
+                grandparent = node2->parent->parent;
 
+                //checking which position the parent is in (left or right)
+                if(grandparent->left == parent_){
+                    Node *node3 = grandparent->right;
+                    
+                    //node 3 is Red
+                    if((node3->color == Red) && (node3 != NULL)){
+                        node3->color = Black;
+                        parent_->color = Black;
+                        grandparent->color = Red;
+                        node2 = grandparent;
+                    }
 
+                    
+                }
+                else{
+
+                }
         }
     }
 
