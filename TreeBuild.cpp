26a82cbac9682dcacb5d4f5cbe42d06890a682f2
Merin Mundt <merin@iastate.edu>
2019-12-06 21:56:45 -0600

    tree work done, testing and parsing text file

--- TreeBuild.cpp
+++ TreeBuild.cpp
@@ -15,8 +15,8 @@ struct Node
     bool color;
     Node *left, *right, *parent;
 
-    Node(){
-        this->data = data;
+    Node(int k){
+        this->data = k;
         this->color = Red;
         right = NULL;
         left = NULL;
@@ -27,19 +27,50 @@ struct Node
 class TreeBuild{    
     private:
         Node *root;
+        Node *NIL;
     public:
         //This is the constructor
         TreeBuild(){
-            root = NULL;
+            NIL = new Node(0);
+            NIL->color = NIL; 
+            root = NIL;
+            root->parent = NIL;
+            root->left = NIL;
+            root->right = NIL;
+
         }
 
     //method to search for a node
     bool searchNode(int data){
-    
+        printf("made it\n");
         Node *curr = root;
-        while (curr != NULL) {
+        while (curr != NIL) {
             if (curr->data == data) {
+                printf("true\n");
                 return true;
+
+            }
+            if (curr->data < data) {
+                curr = curr->right;
+                
+            }
+            if (curr->data > data){
+                curr = curr->left;
+            }
+            
+        }
+        printf("DNE\n");
+        return false;
+    }
+
+    Node* searchNodeReturn(int data){
+        printf("made it\n");
+        Node *curr = root;
+        while (curr != NIL) {
+            if (curr->data == data) {
+                printf("true\n");
+                return curr;
+
             }
             if (curr->data < data) {
                 curr = curr->right;
@@ -49,28 +80,28 @@ class TreeBuild{
             }
             
         }
-        return false;
+        return curr;
     }
 
     //finding the node to replace the node to be deleted
     Node* replaceNode(Node *rep){
         //if node to be deleted has 2 children
-        if(rep->left != NULL && rep->right != NULL){
+        if(rep->left != NIL && rep->right != NIL){
             Node* temp;
             temp = rep->right;
-            while(temp->left != NULL){
+            while(temp->left != NIL){
                 temp = temp->left;
             }
             rep = temp;
             return rep;
         }
         //if node has 2 leaves(NIL) for children
-        if(rep->left == NULL && rep->right == NULL){
-            return NULL;
+        if(rep->left == NIL && rep->right == NIL){
+            return NIL;
         }
 
         //if node only has one child, and one leaf
-        if(rep->left != NULL){
+        if(rep->left != NIL){
             return rep->left;
         }
         else{
@@ -138,14 +169,14 @@ class TreeBuild{
     }
 
     Node* minimum(Node *x){
-          while(x != NULL){
+          while(x != NIL){
               x = x->left;
           }     
           return x;
     }
 
     void Transplant(Node *x, Node *y){
-        if(x->parent == NULL){
+        if(x->parent == NIL){
             root = y;
         }
         else if (x == x->parent->left){
@@ -154,29 +185,53 @@ class TreeBuild{
         else{
             x->parent->right = y;
         }
-        if(y != NULL){
+        if(y != NIL){
             y->parent = x->parent;    
         }
     }
 
     void deleteNode(int data){
-        Node *delNode;
-        Node *dummy;
-        delNode->data = data;
+        Node *delNode = searchNodeReturn(data);
+        if(delNode == NIL){
+            printf("node does not exist\n");
+            return;
+        }
+        Node *dummy = new Node(data);
+        //delNode->data = data;
         Node *y = delNode;
         dummy->color = y->color;
         Node *x;
 
-
-        if(delNode->left == NULL){
+        printf("heee\n");
+        std::cout << root->right << "\n";
+        if(delNode->left == NIL){
+            printf("yes\n");
             x = delNode->right;
+            printf("yes again");
             Transplant(delNode,delNode->right);
         }
-        else if(delNode->right == NULL){
+        else if(delNode->right == NIL){
+            printf("sup\n");
             x = delNode->left;
             Transplant(delNode,delNode->left);
         }
+        // else if(delNode->right == NIL && delNode->left == NIL){
+        //     printf("her\n");
+        //     std::cout << delNode->data << "\n";
+        //     if(delNode->parent->left == delNode){
+                
+        //         printf("ok");
+        //         delNode->parent->left = NULL;
+        //     }
+        //     else{
+        //         printf("idk\n");
+        //         delNode->parent->right = NULL;
+        //     }
+        //     //delNode = NULL;
+            
+        //}
         else{
+            printf("mitch\n");
             y = minimum(delNode->right);
             dummy->color = y->color;
             x = y->right;
@@ -194,9 +249,11 @@ class TreeBuild{
             y->color = delNode->color;
 
         }
+        printf("shock\n");
         if(dummy->color == Black){
             fixDelete(x);
         }
+        std::cout << root->right << "\n";
     }
 
 
@@ -205,12 +262,12 @@ class TreeBuild{
             Node *node = root;
             Node *right = node1->right;
             node1->right = right->left;
-            if(node1->right != NULL){
+            if(node1->right != NIL){
                 node1->right->parent = node1;
             }
             //moving up
             right->parent = node1->parent;
-            if(node1->parent == NULL){
+            if(node1->parent == NIL){
                 node = right;
             }
             else if(node1->parent->left == node1){
@@ -228,12 +285,12 @@ class TreeBuild{
             Node *node = root;
             Node *left = node1->left;
             node1->left = left->right;
-            if(node1->left != NULL){
+            if(node1->left != NIL){
                 node1->left->parent = node1;
             }
             //moving up
             left->parent = node1->parent;
-            if(node1->parent == NULL){
+            if(node1->parent == NIL){
                 node = left;
             }
             else if(node1->parent->right == node1){
@@ -247,37 +304,42 @@ class TreeBuild{
     }
 
     //Method to insert Node data1 in parameter into tree
-    Node* insertNode(int data1){
+    void insertNode(int data1){
+        printf("in here\n");
         Node *node = root;
-        Node *newNode;
-        newNode->data = data1;
-        if(node == NULL){
-            return newNode;
-        }
-        else{
-            int num = node->data;  
-            bool insert = false; 
-            while(!insert){     
-                //percolating node down to its position
-                if(data1 < num){
-                    node = node->left;
-                    if(node == NULL){
-                        node->parent->left = newNode;
-                    }
-                }
-                else if(data1 > num){
-                    node = node->right;
-                    if(node == NULL){
-                        node->parent->right = newNode;
-                    }
-                }
-                    
+        Node *y = NIL;
+        Node *newNode = new Node(data1);
+        while(node != NIL){
+            y = node;
+            if(newNode->data < node->data){
+                node = node->left;
+            }
+            else{
+                node = node->right;
             }
         }
+        newNode->parent = y;
+        if(y == NIL){
+            root = newNode;
+            root->parent = NIL;
+            root->color = Black;
 
-        //might not be right here
-        refactorTree(node);
-        return node;
+        }
+        else if(y->data < newNode->data){
+            y->right = newNode;
+        }
+        else{
+            y->left = newNode;
+        }
+        
+        newNode->left = NIL;
+        newNode->right = NIL;
+        newNode->color = Red;
+
+        std::cout << root->data << "\n";
+        std::cout << root->color << "\n";
+        refactorTree(newNode);
+        
 
     }
     //method to make sure the tree follows all Red Black Tree Properties
@@ -328,7 +390,7 @@ class TreeBuild{
                     Node *node4 = grandparent->left;
 
                     //node 4 is red
-                    if(node4->color == Red && node4 != NULL){
+                    if(node4->color == Red && node4 != NIL){
                         node4->color = Black;
                         parent_->color = Black;
                         grandparent->color = Red;
@@ -357,27 +419,48 @@ class TreeBuild{
         node->color = Black;
     }
 
-    void parseInput(string file){
-        
-        // std::vector<string> commands;
-        // string line;
-        // ifstream inputFile(file);
-        // if(inputFile.is_open()){
-            // while(!EOF){
-            //     getline(inputFile, line);
-            //     //std::cout << line << ' ';
-            //     commands.push_back(line);
-            // }
-        //}
-        // for(int i = 0; i < commands.size(); i++){
-        //     std::cout << commands[i] << ' ';
-        // }
-    }
 };
+
+
+vector<string> parseInput(string file){
+    
+    std::vector<string> commands;
+    string line;
+    //printf("Here\n");
+    ifstream inputFile(file);
+    if(inputFile.is_open()){
+        //printf("herr2\n");
+        while(!inputFile.eof()){
+            printf("here3\n");
+            getline(inputFile, line);
+            commands.push_back(line);
+        }
+        inputFile.close();
+    }
+    for(int i = 0; i < commands.size(); i++){
+        std::cout << commands[i] << '\n';
+    }
+    return commands;
+}
+
+
 
 int main(int i){
         TreeBuild Tree;
-        Tree.parseInput("Hello\0");
-        Tree.insertNode(10);
+        vector<string> commands;
+        commands = parseInput("Hello.txt");
+        for(int i = 0; i < commands.size(); i++){
+            string instruction =commands[i];
+        }   
+        // Tree.insertNode(10);
+        // Tree.insertNode(13);
+        // Tree.insertNode(5);
+        // Tree.insertNode(8);
+        // Tree.insertNode(19);
+        // Tree.searchNode(5);
+        // printf("got here\n");
+        // Tree.deleteNode(13);
+        // Tree.searchNode(13);
+        
         return 0;
 }
